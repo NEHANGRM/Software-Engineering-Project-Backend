4ab5a47011cea1fe33bf183e9d993d45bdefa773
Nehan <nehangrm123@gmail.com>
2026-02-09 01:33:32 +0530

    feat: Connect to cloud database and standardize config

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    console.log('Mongo URI:', process.env.MONGO_URI);
+    console.log('Mongo URI:', process.env.MONGODB_URI);
 
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
 
     console.log('MongoDB connected');
   } catch (error) {
